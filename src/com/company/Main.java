aad614d3fcbbeed7a9ea3be9270e1a41d6e9fae8
ortemmetro <ortem.koldun@gmail.com>
2021-03-10 11:14:11 +0600

    No

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,23 @@
 package com.company;
 
 import com.company.controllers.EnemyController;
-import com.company.entities.Enemy;
+import com.company.data.PostgresDB;
+import com.company.data.interfaces.IDB;
 import com.company.repositories.EnemyRepositories;
 import com.company.repositories.interfaces.IEnemyRepositories;
 
 public class Main {
 
-    private static MyApplication app;
+
 
     public static void main(String[] args) {
+
 //        IDB db = new PostgresDB();
 //        IEnemyRepositories repo = new EnemyRepositories(db);
 //        EnemyController controller = new EnemyController(repo);
 //        MyApplication app = new MyApplication(controller);
-        app.start(); //starting the My Application
+        MyApplication.difficulty();
+        MyApplication.start(); //starting the My Application
+
     }
 }
